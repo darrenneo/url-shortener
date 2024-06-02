54db70486b211a6f2937edb4098e5abd161f9132
darrenneo <darren0872@gmail.com>
2024-06-02 18:41:10 +0800

    feat: Added init for storage map

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"url-shortener/internal/router"
+	"url-shortener/internal/storage"
 )
 
 func main() {
 	e := router.InitRouter()
+	storage.InitStorage()
 	e.Logger.Fatal(e.Start("localhost:8000"))
 }
