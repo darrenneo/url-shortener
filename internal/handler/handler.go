5074408ddfb418b0fe01b92150ea718934430b05
darrenneo <darren0872@gmail.com>
2024-06-02 18:41:42 +0800

    feat: Added logic for ShortenURL handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,10 +1,36 @@
 package handler
 
-import "github.com/labstack/echo/v4"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"url-shortener/internal/model"
+	"url-shortener/internal/storage"
+
+	"github.com/labstack/echo/v4"
+)
 
 func ShortenURL(c echo.Context) error {
-	url := c.Param("url")
-	return c.String(200, url)
+	if c.Request().Body == nil {
+		return c.JSON(http.StatusBadRequest, "Missing body")
+	}
+
+	rawData, err := io.ReadAll(c.Request().Body)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "Invalid body")
+	}
+
+	apiRequest := model.ApiRequest{}
+	json.Unmarshal(rawData, &apiRequest)
+
+	err = storage.AddURL(apiRequest.OriginalURL, apiRequest.NewURL)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	storage.ListAll()
+
+	return nil
 }
 
 func RedirectURL(c echo.Context) error {
