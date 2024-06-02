a0bf93e3044f6f91867d6d920f43a111d4f367de
darrenneo <darren0872@gmail.com>
2024-06-02 18:43:32 +0800

    feat: Added adding of url to map and checking if key already exists

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,11 +1,32 @@
 package storage
 
+import "fmt"
+
 var urlMap map[string]string
 
 func InitStorage() {
 	urlMap = make(map[string]string)
 }
 
-func AddURL(orginal, new string) {
+func AddURL(orginal, new string) error {
+	exist := checkExisting(new)
+
+	if exist {
+		return fmt.Errorf("new URL name exist")
+	}
+
 	urlMap[new] = orginal
+
+	return nil
+}
+
+func ListAll() {
+	for key, value := range urlMap {
+		fmt.Printf("Key: %s, Value: %s\n", key, value)
+	}
+}
+
+func checkExisting(key string) bool {
+	_, ok := urlMap[key]
+	return ok
 }
