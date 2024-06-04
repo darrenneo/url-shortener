240fe3cfeeaff9e1922c74bf1f13b91b6fb25a39
darrenneo <darren0872@gmail.com>
2024-06-05 02:09:18 +0800

    feat: Added checking of CORS, Gzip and secure middleware

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,15 +1,28 @@
 package router
 
 import (
+	"net/http"
+
 	"url-shortener/internal/handler"
 
 	"github.com/labstack/echo/v4"
+	echoMiddleware "github.com/labstack/echo/v4/middleware"
 )
 
 func InitRouter() *echo.Echo {
 	e := echo.New()
 	e.HideBanner = true
 	e.HidePort = true
+
+	e.Use(
+		echoMiddleware.CORSWithConfig(echoMiddleware.CORSConfig{
+			AllowOrigins: []string{"*"},
+			AllowMethods: []string{http.MethodGet, http.MethodPost},
+		}),
+		echoMiddleware.Gzip(),
+		echoMiddleware.Secure(),
+	)
+
 	attachRoutes(e)
 	return e
 }
